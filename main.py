53cd92429f3c98e2f1c1bf107820ceddd9487960
chiefkarim <krimotop2016@gmail.com>
2026-01-04 12:28:32 +0100

    feat: basic genai client with genrate content method - default model gemini-2.5-flash can be changed form env MODEL

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("GEMINI_API_KEY not found")
+model = os.getenv("MODEL")
+
+
 def main():
-    print("Hello from ai-agent!")
+    global model
+    model = model if model != None else "gemini-2.5-flash"
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model=model,
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    if response.text != None:
+        print(response.text)
+    else:
+        raise RuntimeError("Somthing went wrong getting response from gemini API")
 
 
 if __name__ == "__main__":
