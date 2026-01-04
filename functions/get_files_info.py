87f862113daf1b5cabc9a45fef9867e708940f79
chiefkarim <krimotop2016@gmail.com>
2026-01-04 18:24:27 +0100

    feat: add more test to the get files info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,18 +3,25 @@ import os
 
 def get_files_info(working_directory: str, directory: str = "."):
     try:
+        result = (
+            "Result for current directory:\n"
+            if directory == "."
+            else f"Result for '{directory}' directory:\n"
+        )
         abs_working_dir = os.path.realpath(working_directory)
         abs_target_dir = os.path.realpath(os.path.join(abs_working_dir, directory))
         if not os.path.isdir(abs_target_dir):
-            return f'Error: "{abs_target_dir}" is not a directory'
+            return result + f'Error: "{directory}" is not a directory'
 
         valid_target_dir = abs_working_dir == os.path.commonpath(
             [abs_target_dir, abs_working_dir]
         )
         if valid_target_dir != True:
-            return f'Error: Cannot list "{abs_target_dir}" as it is outside the permitted working directory'
+            return (
+                result
+                + f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            )
 
-        result = "Result for current directory:\n"
         with os.scandir(abs_target_dir) as it:
             for item in it:
                 result += f"- {item.name}: file_size={item.__sizeof__()} bytes, is_dir={item.is_dir()}\n"
--- functions/test_get_files_info.py
+++ functions/test_get_files_info.py
@@ -18,13 +18,27 @@ class TestGetFilesInfo(unittest.TestCase):
         result = str.split(get_files_info("calculator", "pkg"), "\n")
 
         self.assertTrue(3 <= len(result))
-        self.assertIn("Result for current directory:", result[0])
+        self.assertIn("Result for 'pkg' directory:", result[0])
         joined_res = str.join("\n", result)
         self.assertRegex(
             joined_res,
             r"- calculator.py: file_size=\d+ bytes, is_dir=False",
         )
         self.assertRegex(joined_res, r"- render.py: file_size=\d+ bytes, is_dir=False")
+
+    def test_calculator_bin(self):
+        result = get_files_info("calculator", "/bin")
+        self.assertEqual(
+            result,
+            "Result for '/bin' directory:\nError: Cannot list \"/bin\" as it is outside the permitted working directory",
+        )
+
+    def test_calculator_parent_dir(self):
+        result = get_files_info("calculator", "../")
+        self.assertEqual(
+            result,
+            "Result for '../' directory:\nError: Cannot list \"../\" as it is outside the permitted working directory",
+        )
 
 
 if __name__ == "__main__":
