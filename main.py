dc76c166c824318803bb56b0f05d75ce1369d099
chiefkarim <krimotop2016@gmail.com>
2026-01-04 14:35:32 +0100

    feat: toggle verose mode with cli args
    - use --verbose flag to show:
        - user token count
        - response token count
        - user prompt

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 
+from google.genai import types
+
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
@@ -17,14 +19,17 @@ def main():
     parser.add_argument(
         "--model", type=str, help="Model defaults to gemini-2.5-flash", required=False
     )
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     model = args.model if args.model != None else "gemini-2.5-flash"
+    prompt = args.user_prompt
+    verbose = args.verbose
 
     client = genai.Client(api_key=api_key)
-    prompt = args.user_prompt
+    messages = [types.Content(parts=[types.Part(text=prompt)], role="user")]
     response = client.models.generate_content(
         model=model,
-        contents=prompt,
+        contents=messages,
     )
     if (
         response.usage_metadata == None
@@ -39,14 +44,24 @@ def main():
         )
     prompt_token_count = response.usage_metadata.prompt_token_count
     response_token_count = response.usage_metadata.candidates_token_count
-
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {response_token_count}")
+    if verbose:
+        printResponseMetadata(
+            prompt,
+            prompt_token_count=prompt_token_count,
+            response_token_count=response_token_count,
+        )
     if response.text != None:
         print(response.text)
     else:
         raise RuntimeError("Somthing went wrong getting response from gemini API")
+
+
+def printResponseMetadata(
+    prompt: str, prompt_token_count: int, response_token_count: int
+):
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {response_token_count}")
 
 
 if __name__ == "__main__":
